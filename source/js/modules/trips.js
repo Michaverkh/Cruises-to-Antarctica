bc7702d696553ad0244e29db18b76783f4004984
Michaverkh <michawerkh@gmail.com>
2022-03-14 22:56:30 +0300

    advantages

---
+++ source/js/modules/trips.js
@@ -0,0 +1,76 @@
+const tripList = [
+    {
+        Id: 0,
+        name: "Астория",
+        route: "Аргентина - Фолклендские острова - Антарктида",
+        start: "Ушуайя",
+        duration: "11",
+        price: "11500",
+        img: "../../img/content/astoria-mobile.jpg"
+ 
+    },
+    {
+        Id: 1,
+        name: "Элайн",
+        route: "Аргентина - Фолклендские острова - Антарктида - Чили",
+        start: "Буэнос-Айрес",
+        duration: "20",
+        price: "14500",
+        img: "../../img/content/astoria-mobile.jpg"
+    },
+    {
+        Id: 2,
+        name: "Атлантис",
+        route: "Аргентина - Южная Георгия и Южные Сандвичевы Острова - Антарктида",
+        start: "Ушуайя",
+        duration: "15",
+        price: "14500",
+        img: "../../img/content/astoria-mobile.jpg"
+    },
+    {
+        Id: 3,
+        name: "Одисей",
+        route: "Аргентина - Южная Георгия и Южные Сандвичевы Острова - Антарктида",
+        start: "Ушуайя",
+        duration: "15",
+        price: "14500",
+        img: "../../img/content/astoria-mobile.jpg"
+    },
+    {
+        Id: 4,
+        name: "Олимпия",
+        route: "Аргентина - Фолклендские острова - Южная Георгия - Антарктида",
+        start: "Ушуайя",
+        duration: "21",
+        price: "18600",
+        img: "../../img/content/astoria-mobile.jpg"
+    },
+    {
+        Id: 5,
+        name: "Аврора",
+        route: "Аргентина - Антарктида - Чили",
+        start: "Ушуайя",
+        duration: "14",
+        price: "11700",
+        img: "../../img/content/astoria-mobile.jpg"
+    },
+    {
+        Id: 6,
+        name: "Леонор",
+        route: "Чили - Южная Георгия - Антарктида - Аргентина",
+        start: "Пунта-Аренас",
+        duration: "17",
+        price: "12500",
+        img: "../../img/content/astoria-mobile.jpg"
+    },
+    {
+        Id: 7,
+        name: "Минион - 1",
+        route: "Аргентина - Антарктида",
+        start: "Ушуайя",
+        duration: "11",
+        price: "12300",
+        img: "../../img/content/astoria-mobile.jpg"
+    }
+]
+
